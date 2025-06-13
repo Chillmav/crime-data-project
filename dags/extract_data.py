2cccf9b6e6afd4d1c668cf9e3cfd11ed3a9f775a
WiktorSzajerka <wszajerka@wp.pl>
2025-06-13 16:41:12 +0200

    działa

--- dags/extract_data.py
+++ dags/extract_data.py
@@ -21,10 +21,9 @@ def etl():
     @task
 
     def extract_crime():
-
-        crime_df = pd.read_csv("/usr/local/airflow/include/Crime_Data_from_2020_to_Present_20250607.csv", usecols=["Vict Age", "Vict Sex", "Vict Descent", "Date Rptd", "DATE OCC", "Time OCC", "Weapon Used Cd", "AREA NAME", "Rpt Dist No", "LOCATION", "Crm Cd Desc", "Weapon Used Cd", "Weapon Desc", "Status Desc"]) # crime data
-
-        return crime_df
+        crime_df = pd.read_csv("/usr/local/airflow/include/Crime_Data_from_2020_to_Present_20250612.csv", usecols=["Vict Age", "Vict Sex", "Vict Descent", "Date Rptd", "DATE OCC", "TIME OCC", "Weapon Used Cd", "AREA NAME", "Rpt Dist No", "LOCATION", "Crm Cd Desc", "Weapon Used Cd", "Weapon Desc", "Status Desc"]) # crime data
+        crime_df.to_csv("/usr/local/airflow/include/_extracted_crime_data.csv", index=False)
+        return
 
     @task
 
@@ -61,7 +60,10 @@ def etl():
         return bezdomni
 
     @task
-    def transform_to_dim_terytorium(data):
+    def transform_to_dim_terytorium():
+
+        data = pd.read_csv("/usr/local/airflow/include/_extracted_crime_data.csv")
+
         terytorium = data[["AREA NAME", "Rpt Dist No", "LOCATION"]]
         terytorium.dropna()
         terytorium.rename(columns={"AREA NAME": "Obszar", "Rpt Dist No": "Dzielnica", "LOCATION": "Ulica"}, inplace=True)
@@ -70,34 +72,36 @@ def etl():
         area_dist.insert(2, "Ulica", 'nieznana')
         area = terytorium["Obszar"].drop_duplicates()
         area = area.to_frame()
-        area.insert(1, "Dzielnica", "nieznana")
+        area.insert(1, "Dzielnica", 0)
         area.insert(2,"Ulica", "nieznana")
-        terytorium["id_terytorium"] = terytorium.index + 2
-        terytorium = pd.concat([pd.DataFrame([["nieznany", 0, "nieznana", 1]], columns=terytorium.columns), terytorium, area_dist, area])
-
+        terytorium = pd.concat([pd.DataFrame([["nieznany", 0, "nieznana"]], columns=terytorium.columns), terytorium, area_dist, area])
+        terytorium.reset_index(drop=True, inplace=True)
+        terytorium["id_terytorium"] = terytorium.index + 1
         
         return terytorium
 
     @task
-    def transform_to_facts(data, dim_ofiara, dim_terytorium, dim_data, dim_sczegoly_przestepstwa, dim_bezdomni):
+    def transform_to_facts(dim_ofiara, dim_terytorium, dim_data, dim_sczegoly_przestepstwa, dim_bezdomni):
+        data = pd.read_csv("/usr/local/airflow/include/_extracted_crime_data.csv")
+       
         # transform dates to year, month, week, day
-        data["Date Rptd"] = pd.to_datetime(data["Date Rptd"], format="%m, %d, %y")
+        data["Date Rptd"] = pd.to_datetime(data["Date Rptd"])
         data['rok_rp'] =  pd.DatetimeIndex(data['Date Rptd']).year
         data["kwartal_rp"] = pd.DatetimeIndex(data['Date Rptd']).quarter
         data['miesiac_rp'] = pd.DatetimeIndex(data['Date Rptd']).month     
         data['dzien_rp'] = data['Date Rptd'].dt.day_name()
         data['tydzien_rp'] = data["Date Rptd"].dt.isocalendar().week
 
-        data["Date oc"] =  pd.to_datetime(data["Date OCC"], format="%m, %d, %y")
-        data['rok_oc'] =  pd.DatetimeIndex(data['Date CC']).year
-        data["kwartal_oc"] = pd.DatetimeIndex(data['Date Rptd']).quarter
-        data['miesiac_oc'] =  pd.DatetimeIndex(data['Date OCC']).month
-        data['dzien_oc'] =  data['Date OCC'].dt.day_name()
-        data['tydzien_oc'] = data["Date OCC"].dt.isocalendar().week
+        data["DATE OCC"] =  pd.to_datetime(data["DATE OCC"])
+        data['rok_oc'] =  pd.DatetimeIndex(data['DATE OCC']).year
+        data["kwartal_oc"] = pd.DatetimeIndex(data['DATE OCC']).quarter
+        data['miesiac_oc'] =  pd.DatetimeIndex(data['DATE OCC']).month
+        data['dzien_oc'] =  data['DATE OCC'].dt.day_name()
+        data['tydzien_oc'] = data["DATE OCC"].dt.isocalendar().week
         data["pora_dnia"] = "noc"
         data["pora_dnia"] = data["pora_dnia"].where(data["TIME OCC"] < 500, "rano")
         data["pora_dnia"] = data["pora_dnia"].where(data["TIME OCC"] < 1100, "południe")
-        data["pora_dnia"] = data["pora_dnia"].where(data["TIME OCC"] < 1800, "wieczorem")
+        data["pora_dnia"] = data["pora_dnia"].where(data["TIME OCC"] < 1800, "wieczór")
         data["pora_dnia"] = data["pora_dnia"].where(data["TIME OCC"] < 2300, "noc")
 
         dni_po_polsku = {
@@ -114,25 +118,23 @@ def etl():
         data["dzien_rp"] = data["dzien_rp"].map(dni_po_polsku)
 
         # calculate time between occ and rptd
-        data["czas_zgloszenia"] = data["Date Rptd"] - data["Date OCC"]
+        data["czas_zgloszenia"] = data["Date Rptd"] - data["DATE OCC"]
         data["czas_zgloszenia"] = data["czas_zgloszenia"].dt.days
-        data.drop(columns=["Date Rptd", "Date OCC", "TIME OCC"], inplace=True)
+        data.drop(columns=["Date Rptd", "DATE OCC", "TIME OCC"], inplace=True)
 
         rows = []
         for index, row in data.iterrows():
 
-            rows.append({
-                "id_szczegoly_przestepstwa": index,
-                "typ": klasyfikuj_typ_przestepstwa(row["Crm Cd Desc"]),
-                "rodzaj_broni": klasyfikuj_rodzaj_broni(row["Weapon Desc"]),
-                "status": klasyfikuj_status(row["Status Desc"])
-            })
+            rows.append([
+                klasyfikuj_typ_przestepstwa(row["Crm Cd Desc"]),
+                klasyfikuj_rodzaj_broni(row["Weapon Desc"]),
+                klasyfikuj_status(row["Status Desc"])
+            ])
 
-            index += 1
 
         data[["Crm Cd Desc", "Weapon Desc", "Status Desc"]] = rows
 
-        data.rename({"Crm Cd Desc":"typ", "Weapon Desc":"rodzaj_broni", "Status Desc":"status"}, inplace=True)
+        data.rename(columns={"Crm Cd Desc":"typ", "Weapon Desc":"rodzaj_broni", "Status Desc":"status"}, inplace=True)
 
         danger_dict = {
             "Zabójstwa":5,
@@ -146,8 +148,8 @@ def etl():
         }
 
         # calculate danger level
+        data['Weapon Used Cd'].fillna(0, inplace=True)
         data['st_niebezpieczenstwa'] = data['Weapon Used Cd'].astype(str).str[0].astype(int)
-        data['st_niebezpieczenstwa'].fillna(0)
         data["typ_nr"] = data["typ"].map(danger_dict)
         data['st_niebezpieczenstwa'] = data['st_niebezpieczenstwa'] + data["typ_nr"]
         data.drop(columns=["typ_nr", "Weapon Used Cd"], inplace=True)
@@ -176,30 +178,30 @@ def etl():
 
         data["Vict Descent"] = data["Vict Descent"].map(ethnicity_dict)
         data["Vict Sex"] = data["Vict Sex"].where(data["Vict Sex"].isin(["M", "F"]), "nieznana")
-        
-        data.rename({"Vict Descent": "rasa", "Vict Age": "wiek", "Vict Sex":"plec"}, inplace=True)
+
+        data.rename(columns={"Vict Descent": "rasa", "Vict Age": "wiek", "Vict Sex":"płeć"}, inplace=True)
         
         # create fact table
-        facts = pd.DataFrame(columns=["czas_zgloszenia", "st_niebezpieczenstwa", "id_data_zp", "id_data_pp", "id_terytorium", "id_ofiara", "id_szcegoly_przestepstwa", "id_bezdomni"])
+        facts = pd.DataFrame(columns=["czas_zgloszenia", "st_niebezpieczenstwa", "id_data_zp", "id_data_pp", "id_terytorium", "id_ofiary", "id_szczegoly_przestepstwa", "id_bezdomni"])
         facts["czas_zgloszenia"] = data["czas_zgloszenia"]
         facts["st_niebezpieczenstwa"] = data["st_niebezpieczenstwa"]
 
         merged_df = pd.merge(data[["rok_rp", "miesiac_rp", "dzien_rp"]], dim_data[["id_data", "rok", "miesiac", "dzień"]], left_on=["rok_rp", "miesiac_rp", "dzien_rp"], right_on=["rok", "miesiac", "dzień"], how='left')
         merged_df["id_data"].fillna(1)
         facts["id_data_zp"] = merged_df["id_data"]
-        merged_df = pd.merge(data[["rok_oc", "miesiac_rp_oc", "dzien_oc"]], dim_data[["id_data", "rok", "miesiac", "dzień", "pora_dnia"]], left_on=["rok_oc", "miesiac_oc", "dzien_oc", "pora_dnia"], right_on=["rok", "miesiac", "dzień", "pora_dnia"], how='left')
+        merged_df = pd.merge(data[["rok_oc", "miesiac_oc", "dzien_oc", "pora_dnia"]], dim_data[["id_data", "rok", "miesiac", "dzień", "pora_dnia"]], left_on=["rok_oc", "miesiac_oc", "dzien_oc", "pora_dnia"], right_on=["rok", "miesiac", "dzień", "pora_dnia"], how='left')
         merged_df["id_data"].fillna(1)
         facts["id_data_pp"] = merged_df["id_data"]
-        merged_df = pd.merge(data[[["wiek", "plec", "rasa"]]], dim_ofiara[["id_ofiary", "wiek", "płeć", "rasa"]], left_on=[[ "wiek", "plec", "rasa"]], right_on=[["wiek", "płeć", "rasa"]], how='left')
+        merged_df = pd.merge(data[["wiek", "płeć", "rasa"]], dim_ofiara[["id_ofiary", "wiek", "płeć", "rasa"]], left_on=["wiek", "płeć", "rasa"], right_on=["wiek", "płeć", "rasa"], how='left')
         merged_df["id_ofiary"].fillna(1)
         facts["id_ofiary"] = merged_df["id_ofiary"]
-        merged_df = pd.merge(data[["typ", "rodzaj_broni", "status"]], dim_sczegoly_przestepstwa[["id_szczegoly_przestepstwa", "typ", "rodzaj_broni", "status"]], left_on=[[ "typ", "rodzaj_broni", "status"]], right_on=[["typ", "rodzaj_broni", "status"]], how='left')
-        merged_df["id_szcegoly_przestepstwa"].fillna(1)
-        facts["id_szcegoly_przestepstwa"] = merged_df["id_szcegoly_przestepstwa"]
+        merged_df = pd.merge(data[["typ", "rodzaj_broni", "status"]], dim_sczegoly_przestepstwa[["id_szczegoly_przestepstwa", "typ", "rodzaj_broni", "status"]], left_on=["typ", "rodzaj_broni", "status"], right_on=["typ", "rodzaj_broni", "status"], how='left')
+        merged_df["id_szczegoly_przestepstwa"].fillna(1)
+        facts["id_szczegoly_przestepstwa"] = merged_df["id_szczegoly_przestepstwa"]
         merged_df = pd.merge(data[["AREA NAME"]], dim_bezdomni[["id_bezdomni", "area"]], left_on=["AREA NAME"], right_on=["area"], how='left')
         merged_df["id_bezdomni"].fillna(1)
         facts["id_bezdomni"] = merged_df["id_bezdomni"]
-        merged_df = pd.merge(data[["AREA NAME", "Rpt Dist No", "LOCATION"]], dim_terytorium[["id_terytorium", "Obszar", "Dzielnica", "Ulica"]], left_on=[["AREA NAME", "Rpt Dist No", "LOCATION"]], right_on=[["Obszar", "Dzielnica", "Ulica"]], how='left')
+        merged_df = pd.merge(data[["AREA NAME", "Rpt Dist No", "LOCATION"]], dim_terytorium[["id_terytorium", "Obszar", "Dzielnica", "Ulica"]], left_on=["AREA NAME", "Rpt Dist No", "LOCATION"], right_on=["Obszar", "Dzielnica", "Ulica"], how='left')
         merged_df["id_terytorium"].fillna(1)
         facts["id_terytorium"] = merged_df["id_terytorium"]
 
@@ -289,8 +291,9 @@ def etl():
         return data
 
     @task
-    def transform_to_dim_szczegoly_przestepstwa(data):
+    def transform_to_dim_szczegoly_przestepstwa():
 
+        data = pd.read_csv("/usr/local/airflow/include/_extracted_crime_data.csv")
         columns=["id_szczegoly_przestepstwa", "typ", "rodzaj_broni", "status"]
         rows = []
         index = 1
@@ -311,14 +314,16 @@ def etl():
 
         return crime_details_dim
 
+
     crime_data = extract_crime()
     homeless_data = extract_homeless()
     dim_ofiara = create_dim_ofiara()
     dim_data = create_dim_data()
-    dim_sczegoly_przestepstwa = transform_to_dim_szczegoly_przestepstwa(crime_data)
+    dim_sczegoly_przestepstwa = transform_to_dim_szczegoly_przestepstwa()
     dim_bezdomni = transform_to_dim_bezdomni(homeless_data)
-    dim_terytorium = transform_to_dim_terytorium(crime_data)
-    facts = transform_to_facts(crime_data, dim_ofiara, dim_terytorium, dim_data, dim_sczegoly_przestepstwa, dim_bezdomni)
+    dim_terytorium = transform_to_dim_terytorium()
+    facts = transform_to_facts(dim_ofiara, dim_terytorium, dim_data, dim_sczegoly_przestepstwa, dim_bezdomni)
+    crime_data >> [dim_sczegoly_przestepstwa, dim_terytorium, dim_sczegoly_przestepstwa, dim_ofiara, dim_data]
 
 etl()
 
